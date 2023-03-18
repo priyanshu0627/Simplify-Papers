9c6223fcbfb808b52448ca51c6d2460e4cf39710
Priyanshu-AI <priyanshu0627@gmail.com>
2023-03-18 22:36:21 +0530

    add ask a question card

---
+++ src/components/AskQuestionCard.tsx
@@ -0,0 +1,90 @@
+import { Avatar, Button, TextField } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import type { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
+import React from 'react';
+
+function AskQuestionCard() {
+  const [channelName, setChannelName] = React.useState('');
+  const [questionTitle, setQuestionTitle] = React.useState('');
+  const [questionComment, setQuestionComment] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setChannelName(event.target.value as string);
+  };
+  const handleQuestionTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestionTitle(event.target.value as string);
+  };
+  const handleComment = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setQuestionComment(event.target.value as string);
+  };
+
+  return (
+    <div className="p-2">
+      <div className="flex items-center justify-between	">
+        <div className="flex items-center">
+          <Avatar>P</Avatar>
+          <p className="mx-4 text-sm">Public Channel</p>
+        </div>
+        <FormControl fullWidth className="flex-1">
+          <InputLabel id="demo-simple-select-label">Channel</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={channelName}
+            label="Channel"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className="flex">
+        <div className="pt-[55px]">
+          <Avatar>A</Avatar>
+        </div>
+        <div className="relative top-[55px] left-[15px] h-4 w-4 -translate-x-1/2 translate-y-1/2 rotate-45 border-l border-b border-gray-300 bg-white"></div>{' '}
+        <div className="flex-1 rounded-md shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+          <div className=" rounded-t-md bg-[#2cabe2]	 px-4 text-sm text-white">
+            Pubic Channel
+          </div>
+          <div className="mb-2 px-2">
+            <TextField
+              color="primary"
+              label="Question"
+              placeholder="Ask a Question"
+              fullWidth={true}
+              required={true}
+              value={questionTitle}
+              onChange={handleQuestionTitle}
+              className="mt-4"
+            />
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Comment"
+              placeholder="Add a Comment (Optional)"
+              fullWidth={true}
+              multiline
+              maxRows={4}
+              value={questionComment}
+              onChange={handleComment}
+              className="mt-4"
+            />
+            <div className="mt-4 text-sm">
+              Use MarkDown to enrich this comment.
+            </div>
+            <div className="mt-4 flex flex-row-reverse">
+              <Button variant="contained">ADD QUESTION</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AskQuestionCard;
--- src/components/PDFSideBar.tsx
+++ src/components/PDFSideBar.tsx
@@ -1,34 +1,57 @@
+import { Button, OutlinedInput } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { OutlinedInput, Button } from '@mui/material';
+import AskQuestionCard from './AskQuestionCard';
+
 import QuestionCard from './QuestionCard';
 import { QuestionsData } from './QuestionsData';
 
-function PDFSideBar({onFileChange} : any) {
+function PDFSideBar({ onFileChange }: any) {
   // const [questionsData, setQuestionsData] = useState([]);
+  const [questionToggle, setQuestionToggle] = useState(false);
+
+  const handleAskQuestionButton = () => {
+    setQuestionToggle(!questionToggle);
+  };
   useEffect(() => {
     // set questions data from the api here
-  }, [])
-  
+  }, []);
 
   return (
-    <div className='flex flex-col justify-start p-1 m-4 rounded-lg bg-white text-black h-screen'>
+    <div className="m-4 flex h-screen flex-col justify-start rounded-lg bg-white p-1 text-black">
       {/* <section className='flex justify-center'>
         <div className='my-4 py-4 bg-white rounded-lg w-9/12'>
           <label className='m-1' htmlFor="file">Load from file:</label>{" "}
           <input className='m-1' onChange={onFileChange} type="file" />
         </div>
       </section> */}
-      <section className='flex justify-around'>
-        <Button className='text-white bg-theme-green flex-auto m-1' sx={{background: '#45815a'}} size="large" variant="contained">Ask a Question</Button>
-        <OutlinedInput className='flex-auto m-1' placeholder='Search these discussion'/>
+      <section className="flex justify-around">
+        <Button
+          className="m-1 flex-auto bg-theme-green text-white"
+          sx={{ background: '#45815a' }}
+          size="large"
+          variant="contained"
+          onClick={handleAskQuestionButton}
+        >
+          {questionToggle ? 'Ask a Question' : 'All Questions'}
+        </Button>
+        <OutlinedInput
+          className="m-1 flex-auto"
+          placeholder="Search these discussion"
+        />
       </section>
-      <section className='overflow-auto '>
-        {QuestionsData?.map((questionData: any) => (
-          <QuestionCard {...questionData} />
-        ))}
-      </section>
+      {questionToggle ? (
+        <section className="overflow-auto ">
+          {QuestionsData?.map((questionData: any) => (
+            <QuestionCard key={questionData.id} {...questionData} />
+          ))}
+        </section>
+      ) : (
+        <section>
+          <AskQuestionCard />
+        </section>
+      )}
     </div>
-  )
+  );
 }
 
 export default PDFSideBar;
