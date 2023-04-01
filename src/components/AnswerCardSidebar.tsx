8571879ce552a27e197bbf9bc82d3caeffadc8e8
Priyanshu-AI <priyanshu0627@gmail.com>
2023-04-02 01:39:14 +0530

    add ans card
    add support for ans card in pdf side bar, question data update

---
+++ src/components/AnswerCardSidebar.tsx
@@ -0,0 +1,289 @@
+import { RemoveRedEye } from '@mui/icons-material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShareIcon from '@mui/icons-material/Share';
+import SpellcheckIcon from '@mui/icons-material/Spellcheck';
+import { Avatar, Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
+
+const data: any = {
+  id: 1,
+  upVotes: 4,
+  downVotes: 2,
+  liked: 3,
+  accepted: true,
+  flag: 5,
+  views: 30,
+  question: 'How did you do this ?',
+  askedBy: 'mr-easy',
+  lastActivityTime: '5 mins',
+  lastActivityPerson: 'Rishab',
+  tags: ['React', 'next'],
+};
+
+function AnswerCardSidebar() {
+  const [bookmark, setBookmark] = useState(false);
+  const [upVote, setUpVote] = useState(data.upVotes);
+  const [downVote, setDownVote] = useState(data.downVotes);
+  const [like, hitLike] = useState(false);
+  const [userAnswere, setUserAnwere] = useState('');
+  const [flag, setFlag] = useState(data.flag);
+  const handleShareProject = () => {
+    setBookmark(!bookmark);
+  };
+
+  const handleUpVote = () => {
+    setUpVote(upVote + 1);
+  };
+
+  const handleDownVote = () => {
+    if (downVote - 1 >= 1) {
+      setDownVote(downVote - 1);
+    }
+  };
+
+  const handleFlag = () => {
+    // setFlag(flag + 1);
+  };
+
+  const handleLikeButton = () => {
+    hitLike(!like);
+  };
+
+  const handleCheckAnswer = () => {};
+  const handleSubmitAns = () => {};
+
+  return (
+    <div className="flex flex-col p-3">
+      <section className="border-b-2 border-solid border-theme-green">
+        Summary Of the Thing
+      </section>
+
+      <section className="my-4 flex flex-row justify-between border-b-2 border-solid border-theme-green pb-4">
+        <div className="flex w-[88%] flex-col">
+          <div className="my-1 text-lg">What is Lorem Ipsum?</div>
+          <div className="text-xs text-gray-700">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </div>
+        </div>
+        <div className="ml-3 flex	w-[12%] flex-col justify-start">
+          <div className="flex items-center	justify-between">
+            <div className="text-xs text-gray-700">{upVote}</div>
+            <ArrowDropUpIcon
+              fontSize="large"
+              className="text-theme-green"
+              onClick={handleUpVote}
+            />
+          </div>
+          <div className="flex items-center	justify-between">
+            <div className="text-xs text-gray-700">{data.liked}</div>
+            {like ? (
+              <FavoriteBorderIcon
+                fontSize="medium"
+                className="mr-1 text-theme-green"
+                onClick={handleLikeButton}
+              />
+            ) : (
+              <FavoriteIcon
+                fontSize="medium"
+                className="mr-1 text-theme-green"
+                onClick={handleLikeButton}
+              />
+            )}
+          </div>
+          <div className="flex items-center	justify-between">
+            <div className="text-xs text-gray-700">{downVote}</div>
+            <ArrowDropDownIcon
+              fontSize="large"
+              className="text-theme-green"
+              onClick={handleDownVote}
+            />
+          </div>
+          <div className="flex items-center	justify-between">
+            <div className="text-xs text-gray-700">{flag}</div>
+            {true ? (
+              <SpellcheckIcon
+                className="text-theme-green"
+                onClick={handleFlag}
+              />
+            ) : (
+              <CheckBoxOutlineBlankIcon
+                className="text-theme-green"
+                onClick={handleCheckAnswer}
+              />
+            )}
+          </div>
+          <div className="flex items-center	justify-between">
+            <div className="text-xs text-gray-700">{data.views}</div>
+            <RemoveRedEye fontSize="small" className="text-theme-green" />
+          </div>
+          <div className="flex items-center	justify-end	">
+            <ShareIcon className="text-theme-green" />
+          </div>
+        </div>
+      </section>
+
+      <section className="flex flex-col justify-between border-b-2 border-solid border-theme-green pb-4 text-sm">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-col justify-between">
+            <div>
+              {data.tags?.map((tag: string, index: number) => (
+                <Button
+                  key={index}
+                  size="small"
+                  variant="contained"
+                  sx={{ background: '#45815a!important', fontSize: '8px' }}
+                  className="m-1"
+                >
+                  {tag}
+                </Button>
+              ))}
+            </div>
+            <div className="m-1 text-xs text-gray-700">
+              last activity: {data.lastActivityTime} ago by
+              <Button
+                size="small"
+                variant="contained"
+                sx={{ background: '#45815a!important', fontSize: '8px' }}
+                className="m-1"
+              >
+                {data.lastActivityPerson}
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col rounded-xl border-2 border-solid border-theme-green bg-theme-lightGreen p-1 text-gray-700">
+            <div className="flex flex-row items-center justify-between">
+              <div className="text-[11px]">asked by</div>
+              <Button
+                size="small"
+                variant="contained"
+                sx={{ background: '#45815a!important', fontSize: '8px' }}
+                className="m-1"
+              >
+                {data.askedBy}
+              </Button>
+            </div>
+            <div className="flex flex-row">
+              <Avatar />
+              <div className="flex flex-col justify-center">
+                <div className="text-[11px]">30k points</div>
+                <div className="text-[11px]">20 Sept 2022</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="my-4 flex flex-col justify-between border-b-2 border-solid border-theme-green pb-4 text-sm">
+        <TextField
+          required
+          id="answer"
+          label="Answer"
+          maxRows={4}
+          placeholder="Type Your Ans Here..."
+          value={userAnswere}
+          sx={{ width: '100%' }}
+          variant="filled"
+          color="success"
+          focused
+        />
+        <Button onClick={handleSubmitAns} variant="outlined" className="mt-2">
+          Submit
+        </Button>
+      </section>
+
+      <section className="my-4 flex flex-col justify-between pb-4 text-sm">
+        <div>2 Answers</div>
+        <div className="my-2 flex flex-row rounded-lg border-2 border-solid border-theme-green py-2">
+          <div className="mr-3 flex w-[15%] flex-col justify-start">
+            <div className="flex items-center	justify-around">
+              <ArrowDropUpIcon
+                fontSize="large"
+                className="text-theme-green"
+                onClick={handleUpVote}
+              />
+              <div className="text-xs text-gray-700">{upVote}</div>
+            </div>
+            <div className="flex items-center	justify-around	">
+              <ArrowDropDownIcon
+                fontSize="large"
+                className="text-theme-green"
+                onClick={handleDownVote}
+              />
+              <div className="text-xs text-gray-700">{downVote}</div>
+            </div>
+            <div className="ml-2 flex	items-center justify-start">
+              <ShareIcon className="text-theme-green" />
+            </div>
+          </div>
+          <div className="flex w-[85%] flex-col">
+            <div className="text-lg">Ans Heading</div>
+            <div className="text-xs text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </div>
+          </div>
+        </div>
+        <div className="my-2 flex flex-row rounded-lg border-2 border-solid border-theme-green py-2">
+          <div className="mr-3 flex w-[15%] flex-col justify-start">
+            <div className="flex items-center	justify-around">
+              <ArrowDropUpIcon
+                fontSize="large"
+                className="text-theme-green"
+                onClick={handleUpVote}
+              />
+              <div className="text-xs text-gray-700">{upVote}</div>
+            </div>
+            <div className="flex items-center	justify-around	">
+              <ArrowDropDownIcon
+                fontSize="large"
+                className="text-theme-green"
+                onClick={handleDownVote}
+              />
+              <div className="text-xs text-gray-700">{downVote}</div>
+            </div>
+            <div className="ml-2 flex	items-center justify-start">
+              <ShareIcon className="text-theme-green" />
+            </div>
+          </div>
+          <div className="flex w-[85%] flex-col">
+            <div className="text-lg">Ans Heading</div>
+            <div className="text-xs text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default AnswerCardSidebar;
--- src/components/PDFSideBar.tsx
+++ src/components/PDFSideBar.tsx
@@ -1,55 +1,98 @@
 import { Button, OutlinedInput } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import AskQuestionCard from './AskQuestionCard';
 
+import type { QuestionsDataType } from '../Types/Types';
+import AnswerCardSidebar from './AnswerCardSidebar';
+import AskQuestionCard from './AskQuestionCard';
 import QuestionCard from './QuestionCard';
 import { QuestionsData } from './QuestionsData';
 
+const SidebarSection = {
+  allQuestions: 'allQuestions',
+  askQuestion: 'askQuestion',
+  seeAnswer: 'seeAnswer',
+};
+
 function PDFSideBar({ onFileChange }: any) {
-  // const [questionsData, setQuestionsData] = useState([]);
-  const [questionToggle, setQuestionToggle] = useState(false);
+  const [sidebarSection, setSidebarSection] = useState(
+    SidebarSection.allQuestions
+  );
 
   const handleAskQuestionButton = () => {
-    setQuestionToggle(!questionToggle);
+    // if sidebarSection state is not allQuestions then set it to all question otherwise if it is allQuestions then set it ask question
+    if (sidebarSection === SidebarSection.allQuestions) {
+      setSidebarSection(SidebarSection.askQuestion);
+    } else {
+      setSidebarSection(SidebarSection.allQuestions);
+    }
   };
+
+  const handleAnswerClick = (questionData: QuestionsDataType) => {
+    console.log(questionData);
+    setSidebarSection(SidebarSection.seeAnswer);
+  };
+
   useEffect(() => {
     // set questions data from the api here
   }, []);
 
   return (
     <div className="m-4 flex h-screen flex-col justify-start rounded-lg bg-white p-1 text-black">
-      {/* <section className='flex justify-center'>
-        <div className='my-4 py-4 bg-white rounded-lg w-9/12'>
-          <label className='m-1' htmlFor="file">Load from file:</label>{" "}
-          <input className='m-1' onChange={onFileChange} type="file" />
+      {/* <section className="flex justify-center">
+        <div className="my-4 w-9/12 rounded-lg bg-white py-4">
+          <label className="m-1" htmlFor="file">
+            Load from file:
+          </label>{' '}
+          <input className="m-1" onChange={onFileChange} type="file" />
         </div>
       </section> */}
       <section className="flex justify-around">
         <Button
           className="m-1 flex-auto bg-theme-green text-white"
-          sx={{ background: '#45815a' }}
+          sx={{ background: '#45815a!important' }}
           size="large"
           variant="contained"
           onClick={handleAskQuestionButton}
         >
-          {questionToggle ? 'Ask a Question' : 'All Questions'}
+          {sidebarSection === SidebarSection.allQuestions
+            ? 'Ask a Question'
+            : 'All Questions'}
         </Button>
         <OutlinedInput
           className="m-1 flex-auto"
           placeholder="Search these discussion"
         />
       </section>
-      {questionToggle ? (
-        <section className="overflow-auto ">
-          {QuestionsData?.map((questionData: any) => (
-            <QuestionCard key={questionData.id} {...questionData} />
-          ))}
-        </section>
-      ) : (
-        <section>
-          <AskQuestionCard />
-        </section>
-      )}
+      {(() => {
+        switch (sidebarSection) {
+          case SidebarSection.allQuestions:
+            return (
+              <section className="overflow-auto">
+                {QuestionsData?.map((questionData: QuestionsDataType) => (
+                  <QuestionCard
+                    key={questionData.id}
+                    {...questionData}
+                    handleSeeAns={() => handleAnswerClick(questionData)}
+                  />
+                ))}
+              </section>
+            );
+          case SidebarSection.askQuestion:
+            return (
+              <section>
+                <AskQuestionCard />
+              </section>
+            );
+          case SidebarSection.seeAnswer:
+            return (
+              <section className="overflow-auto">
+                <AnswerCardSidebar />
+              </section>
+            );
+          default:
+            return null;
+        }
+      })()}
     </div>
   );
 }
