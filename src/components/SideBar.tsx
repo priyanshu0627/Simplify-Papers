58fad7214c14c2a2b8ca3dadf16a1aa8357d51bb
Priyanshu-AI <priyanshu0627@gmail.com>
2023-04-22 23:24:36 +0530

    refactor pdf outline sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -22,9 +22,6 @@ import {
 const SideBar = (props: any) => {
   const { collapseSidebar } = useProSidebar();
   const [bookmark, setBookmark] = useState(false);
-  const [customOutline, setCustomOutline] = useState({});
-  const [customOutlineBool, setCustomOutlineBool] = useState(true);
-  // const [file, setFile] = useState('./file-example_PDF_1MB.pdf');
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
     null
   );
@@ -37,149 +34,11 @@ const SideBar = (props: any) => {
     setAnchorEl(null);
   };
 
-  const testOutlineInput = (ref: any) => {
-    // debugger;
-    if (ref && ref.childNodes && ref.childNodes[0]) {
-      const dummy = testFunction(ref.childNodes[0]);
-      setCustomOutline(dummy);
-      setCustomOutlineBool(false);
-    } else {
-      return;
-    }
-    // console.log(dummy);
-    const jsx = (
-      <SubMenu label="level1">
-        <MenuItem
-          rootStyles={{
-            backgroundColor: '#027983',
-            color: 'white',
-          }}
-        >
-          <a href="#">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac
-            faucibus odio.
-          </a>
-        </MenuItem>
-
-        <SubMenu
-          label="chapter2"
-          rootStyles={{
-            backgroundColor: '#027983',
-            color: 'white',
-          }}
-        >
-          <MenuItem
-            rootStyles={{
-              backgroundColor: '#027983',
-              color: 'white',
-            }}
-          >
-            <a href="#">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac
-              faucibus odio.
-            </a>
-          </MenuItem>
-        </SubMenu>
-      </SubMenu>
-    );
-  };
-
-  const test = (outline: any) => {
-    // console.log(outline);
-    // debugger;
-    // const dummy = testFunction();
-    // console.log(dummy);
-    // setCustomOutline(testFunction());
-  };
-
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
   const handleShareProject = () => {
     setBookmark(!bookmark);
-  };
-
-  const testFunction = (ul: any) => {
-    if (!ul) {
-      return null;
-    }
-    const sidebarOutlineJsx: any = [];
-    ul.childNodes.forEach((element: any) => {
-      sidebarOutlineJsx.push(element.childNodes[0]);
-      if (element.childNodes.length > 1) {
-        testFunction(element.childNodes[1]);
-      }
-    });
-    return { list: sidebarOutlineJsx };
-  };
-
-  // const testFunction = (ul?: any) => {
-  //   if (!ul) {
-  //     if (
-  //       $('.react-pdf__Outline') &&
-  //       $('.react-pdf__Outline').childNodes &&
-  //       $('.react-pdf__Outline').childNodes[0]
-  //     ) {
-  //       ul = $('.react-pdf__Outline').childNodes[0];
-  //     } else {
-  //       return null;
-  //     }
-  //   }
-  //   const sidebarOutlineJsx: any = [];
-  //   ul.childNodes.forEach((element: any) => {
-  //     // console.log(element.childNodes[0]);
-  //     sidebarOutlineJsx.push(
-  //       <MenuItem
-  //         rootStyles={{
-  //           backgroundColor: '#027983',
-  //           color: 'white',
-  //         }}
-  //       >
-  //         {[element.childNodes[0]]}
-  //       </MenuItem>
-  //     );
-  //     if (element.childNodes.length > 1) {
-  //       testFunction(element.childNodes[1]);
-  //     }
-  //   });
-  //   return <SubMenu> {sidebarOutlineJsx} </SubMenu>;
-  // };
-
-  const buildOutlineInSidebar = () => {
-    // $('.react-pdf__Outline').childNodes[0].childNodes.forEach((element) => {
-    //   console.log(element);
-    // });
-
-    const jsx = (
-      <SubMenu label="level1">
-        <MenuItem
-          rootStyles={{
-            backgroundColor: '#027983',
-            color: 'white',
-          }}
-        >
-          chapter 1
-        </MenuItem>
-
-        <SubMenu
-          label="chapter2"
-          rootStyles={{
-            backgroundColor: '#027983',
-            color: 'white',
-          }}
-        >
-          <MenuItem
-            rootStyles={{
-              backgroundColor: '#027983',
-              color: 'white',
-            }}
-          >
-            chapter 2 page 1
-          </MenuItem>
-        </SubMenu>
-      </SubMenu>
-    );
-    return jsx;
   };
 
   useEffect(() => {
@@ -247,22 +106,6 @@ const SideBar = (props: any) => {
               onClick={handleClick}
             />
           </MenuItem>
-          {/* <SubMenu label="Outline">
-            <MenuItem rootStyles={{ '&:hover': { backgroundColor: 'none' } }}>
-              <Document file={props.file} className="flex justify-center">
-                {customOutlineBool && (
-                  <Outline
-                    inputRef={(ref) => testOutlineInput(ref)}
-                    onItemClick={props?.jumpToOutline}
-                    onLoadSuccess={(outline) => test(outline)}
-                  />
-                )}
-              </Document>
-            </MenuItem>
-          </SubMenu> */}
-          {/* {Object.keys(customOutline)?.map((item) =>
-            customOutline[item]?.map((element: any) => <p>{element.text}</p>)
-          )} */}
           <SubMenu
             label="Outline"
             rootStyles={{
@@ -297,23 +140,8 @@ const SideBar = (props: any) => {
                   }
                 />
               </Document>
-              {/* <Outline
-                file={props?.file}
-                onItemClick={props?.jumpToOutline}
-                onLoadError={(error) =>
-                  alert(`Error while retrieving the outline! ${error.message}`)
-                }
-              /> */}
             </MenuItem>
           </SubMenu>
-
-          {/* <section>
-            <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
-              <Drawer variant="persistent" anchor="left" open={true}>
-                <Outline onItemClick={clickTOC} />
-              </Drawer>
-            </Document>
-          </section> */}
         </Menu>
       </Sidebar>
       <main>
