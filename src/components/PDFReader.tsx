0bb5ad04d4405fdf2a5294a3204a423de9c4f965
Priyanshu-AI <priyanshu0627@gmail.com>
2023-05-07 00:45:15 +0530

    add highlight functionality

--- src/components/PDFReader.tsx
+++ src/components/PDFReader.tsx
@@ -1,8 +1,10 @@
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-import React, { useEffect, useState } from 'react';
-import { Document, Outline, Page, pdfjs } from 'react-pdf';
+import $ from 'jquery';
+import dynamic from 'next/dynamic';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 
 import ControlPanel from './ControlPanel';
 import Loader from './Loader';
@@ -17,6 +19,7 @@ const PDFReader = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
+  const [clickTiggered, setClickTriggered] = useState(false);
 
   const clickTOC = ({ pageNumber: itemPageNumber }: any) => {
     setPageNumber(itemPageNumber);
@@ -40,18 +43,20 @@ const PDFReader = () => {
     //   }
     //   return 1;
     // }
-    // function handleHighlightOnClick() {
-    //   console.log('here');
-    // }
     // function iterateAndHighlight(selection: any) {
-    //   let it = selection.anchorNode.parentNode;
-    //   while (it !== selection.focusNode.parentElement) {
-    //     if (it.tagName === 'SPAN') {
-    //       it.innerHTML = `<mark>${it.innerHTML}</mark>`;
-    //     }
-    //     it = it.nextElementSibling;
-    //   }
+    //   debugger;
+    //   console.log(selection);
+    //   const element = $('.react-pdf__Page__textContent.textLayer');
+    //   console.log(element);
+    //   // let it = selection.anchorNode.parentNode;
+    //   // while (it !== selection.focusNode.parentElement) {
+    //   //   if (it && it.tagName && it.tagName === 'SPAN') {
+    //   //     it.innerHTML = `<mark>${it.innerHTML}</mark>`;
+    //   //   }
+    //   //   it = it.nextElementSibling;
+    //   // }
     // }
+
     // function handlerFunction() {
     //   if (document.contains(document.getElementById('share-snippet'))) {
     //     document.getElementById('share-snippet')!.remove();
@@ -60,6 +65,7 @@ const PDFReader = () => {
     //     const selection = window.getSelection();
     //     const range = getRangeObject(selection);
     //     const clientRect = range.getBoundingClientRect();
+
     //     iterateAndHighlight(selection);
     //     const scrollTop =
     //       window.pageYOffset !== undefined
@@ -77,119 +83,172 @@ const PDFReader = () => {
     //     );
     //   }
     // }
-    // document.addEventListener('mouseup', handlerFunction, false);
-    // document.addEventListener('pointerup', (e : any) => {
-    //   debugger;
-    //   const selection = window.getSelection();
-    //         /* get the Selection object */
-    //         const userSelection = window.getSelection();
-    //         /* get the innerText (without the tags) */
-    //         const text = userSelection.toString();
-    //         /* Creating Range object based on the userSelection object */
-    //         var rangeObject = getRangeObject(userSelection);
-    //         /*
-    //            This extracts the contents from the DOM literally, inclusive of the tags.
-    //            The content extracted also disappears from the DOM
-    //         */
-    //         const contents = rangeObject.extractContents();
-    //         var span = document.createElement("span");
-    //         span.className = "highlight";
-    //         span.appendChild(contents);
-    //         /* Insert your new span element in the same position from where the selected text was extracted */
-    //         rangeObject.insertNode(span);
-    //   if (selection.type === 'Range') {
-    //     for (let i = 0; i < selection.rangeCount; i++) {
-    //       const range = selection.getRangeAt(i);
-    //       debugger;
-    //       playAnimation(range.commonAncestorContainer);
-    //     }
-    //   }
-    //   // var selection = window.getSelection();
-    //   // if (!selection.isCollapsed) {
-    //   //     // we have a non-zero length selection
-    //   //     var startNode = selection.anchorNode;
-    //   //     var startOffset = selection.anchorOffset;
-    //   //     if (startNode instanceof Element) {
-    //   //         // if it is an element then the offset is the child node index
-    //   //         startNode = startNode.childNodes[startOffset];
-    //   //         startOffset = 0;
-    //   //     }
-    //   //     var endNode = selection.focusNode;
-    //   //     var endOffset = selection.focusOffset;
-    //   //     if (endNode instanceof Element) {
-    //   //         // if it is an element then the offset is the child node index
-    //   //         endNode = endNode.childNodes[endOffset];
-    //   //         endOffset = 0;
-    //   //     }
-    //   //     if (endNode instanceof Text) {
-    //   //       endNode.splitText(endOffset);
-    //   //   }
-    //   //   if (startNode instanceof Text) {
-    //   //       startNode.splitText(startOffset);
-    //   //       startNode = startNode.nextSibling;
-    //   //   }
-    //   //   // filter out the element with id=ignore
-    //   //   function filterFunction(node) {
-    //   //       if (node.id === 'ignore') {
-    //   //           return NodeFilter.FILTER_REJECT;
-    //   //       }
-    //   //       return NodeFilter.FILTER_ACCEPT;
-    //   //   }
-    //   //   let nodeTypes = NodeFilter.SHOW_ELEMENT|NodeFilter.SHOW_TEXT;
-    //   //   let walker = document.createTreeWalker(document.body, nodeTypes, { acceptNode: filterFunction}, false);
-    //   //   walker.currentNode = startNode;
-    //   //   let nextNode = walker.currentNode;
-    //   //   let html = [];
-    //   //   while (nextNode && nextNode !== endNode) {
-    //   //       if (nextNode.nodeType === Node.TEXT_NODE) {
-    //   //           html.push(nextNode.nodeValue);
-    //   //           // const span = document.createElement("span");
-    //   //           // span.innerHTML = `<mark>${nextNode.nodeValue}</mark>`;
-    //   //           // debugger;
-    //   //           // nextNode.parentElement?.innerHTML(`<mark>${nextNode.nodeValue}</mark>`);
-    //   //       }
-    //   //       debugger;
-    //   //       // nextNode.replace(/e/gi, '<span>e</span>');
-    //   //       nextNode = walker.nextNode();
-    //   //   }
-    //   //   // if (nextNode.nodeType === Node.TEXT_NODE) {
-    //   //   //     html.push(nextNode.nodeValue);
-    //   //   // }
-    //   //   let filteredOutput = document.createElement('li');
-    //   //   filteredOutput.innerHTML = html.join('');
-    //   //     console.log(startNode, startOffset, endNode, endOffset);
-    //   // }
-    // });
-    // function playAnimation(el) {
-    //   if (el.nodeType === Node.TEXT_NODE) {
-    //     el = el.parentNode;
-    //   }
-    //   el.classList.remove('highlight');
-    //   setTimeout(() => {
-    //     el.classList.add('highlight');
-    //   }, 0);
-    // }
-    // function debounce(fn: any, delay: any) {
-    //   let timer: any = null;
-    //   return function () {
-    //     const context = this;
-    //     const args = arguments;
-    //     clearTimeout(timer);
-    //     timer = setTimeout(function () {
-    //       fn.apply(context, args);
-    //     }, delay);
-    //   };
-    // }
-    // document.addEventListener("selectionchange", debounce(function (event) {
+    // function highlightContent() {
+    //   try {
     //     debugger;
-    //   let selection = document.getSelection ? document.getSelection().toString() :  document.selection.createRange().toString() ;
-    //   console.log(selection);
-    // }, 250));
-    // document.addEventListener(
-    //   'selectionchange',
-    //   debounce(handlerFunction, 250)
-    // );
+    //     const metaData: any = {};
+    //     let start = false;
+    //     let end = false;
+    //     // const range: any = window.getSelection();
+    //     const range = document?.getSelection().getRangeAt(0);
+    //     console.log($('.react-pdf__Page__textContent.textLayer')[0].children);
+    //     Object.values(
+    //       $('.react-pdf__Page__textContent.textLayer')[0].children
+    //     ).forEach((element, index) => {
+    //       if (element === range?.startContainer?.parentElement) {
+    //         metaData.startContainerIndex = index;
+    //         if (!start) {
+    //           start = true;
+    //           metaData.reverseHighlight = false;
+    //         } else if (start && !end) {
+    //           end = true;
+    //         }
+    //       }
+    //       if (element === range?.endContainer?.parentElement) {
+    //         metaData.endContainerIndex = index;
+    //         if (!start) {
+    //           start = true;
+    //           metaData.reverseHighlight = true;
+    //         } else if (start && !end) {
+    //           end = true;
+    //         }
+    //       }
+    //       // middle mei hai
+    //       if (start && !end) {
+    //         // markEntireLine(element);
+    //         // const newSpanNode = document.createElement('span');
+    //         const highlightNode = document.createElement('mark');
+    //         highlightNode.textContent = element.textContent;
+    //         element.textContent = '';
+    //         element.appendChild(highlightNode);
+    //         // element.innerHtml = highlightNode.outerHTML;
+    //         // console.log(element.value);
+    //       }
+    //     });
+    //     console.table(metaData);
+    //   } catch (error) {
+    //     throw new Error('Function not implemented.');
+    //   }
+    // }
+    // document.addEventListener('mouseup', highlightContent, false);
   }, []);
+
+  // useEffect(() => {
+  //   function highlightContent() {
+  //     try {
+  //       debugger;
+  //       const metaData: any = {};
+  //       let start = false;
+  //       let end = false;
+  //       const range: any = window.getSelection();
+
+  //       Object.values(
+  //         $('.react-pdf__Page__textContent.textLayer').children
+  //       ).forEach((element, index) => {
+  //         if (element === range?.startContainer.parentElement) {
+  //           metaData.startContainerIndex = index;
+  //           if (!start) {
+  //             start = true;
+  //             metaData.reverseHighlight = false;
+  //           } else if (start && !end) {
+  //             end = true;
+  //           }
+  //         }
+  //         if (element === range.endContainer.parentElement) {
+  //           metaData.endContainerIndex = index;
+  //           if (!start) {
+  //             start = true;
+  //             metaData.reverseHighlight = true;
+  //           } else if (start && !end) {
+  //             end = true;
+  //           }
+  //         }
+  //         // middle mei hai
+  //         if (start && !end) {
+  //           // markEntireLine(element);
+  //           const highlightNode = document.createElement('mark');
+  //           highlightNode.textContent = element.textContent;
+  //           element.innerHtml = highlightNode.outerHTML;
+  //           // console.log(element.value);
+  //         }
+  //       });
+  //       console.table(metaData);
+  //     } catch (error) {
+  //       throw new Error('Function not implemented.');
+  //     }
+  //   }
+  //   highlightContent();
+  //   return () => {};
+  // }, [clickTiggered]);
+
+  const onClick = () => {
+    /** Handle mousedown or click */
+    debugger;
+    // highlightContent();
+  };
+
+  function highlightContent() {
+    try {
+      debugger;
+      const metaData: any = {};
+      let start = false;
+      let end = false;
+      // const range: any = window.getSelection();
+      const range = document?.getSelection().getRangeAt(0);
+      console.log($('.react-pdf__Page__textContent.textLayer')[0].children);
+      Object.values(
+        $('.react-pdf__Page__textContent.textLayer')[0].children
+      ).forEach((element, index) => {
+        if (element === range?.startContainer?.parentElement) {
+          metaData.startContainerIndex = index;
+          if (!start) {
+            start = true;
+            metaData.reverseHighlight = false;
+          } else if (start && !end) {
+            end = true;
+          }
+        }
+        if (element === range?.endContainer?.parentElement) {
+          metaData.endContainerIndex = index;
+          if (!start) {
+            start = true;
+            metaData.reverseHighlight = true;
+          } else if (start && !end) {
+            end = true;
+          }
+        }
+        // middle mei hai
+        if (start && !end) {
+          // markEntireLine(element);
+          // const newSpanNode = document.createElement('span');
+          const highlightNode = document.createElement('mark');
+          highlightNode.textContent = element.textContent;
+          element.textContent = '';
+          element.appendChild(highlightNode);
+          // element.innerHtml = highlightNode.outerHTML;
+          // console.log(element.value);
+        }
+      });
+      console.table(metaData);
+    } catch (error) {
+      throw new Error('Function not implemented.');
+    }
+  }
+
+  const handleMouseUp = (event): any => {
+    if (event.button !== 2) {
+      setClickTriggered(true);
+      setTimeout(() => setClickTriggered(false), 10);
+      onClick();
+      highlightContent();
+    }
+  };
+
+  const handleClick = useCallback(() => {
+    if (!clickTiggered) {
+      onClick();
+    }
+  }, [clickTiggered]);
 
   return (
     <div className="flex bg-gray-900">
@@ -205,13 +264,15 @@ const PDFReader = () => {
             setPageNumber={setPageNumber}
             file={file}
           />
-          <Document
-            file={file}
-            onLoadSuccess={onDocumentLoadSuccess}
-            className="flex justify-center"
-          >
-            <Page pageNumber={pageNumber} scale={scale} />
-          </Document>
+          <section onMouseUp={handleMouseUp} onClick={handleClick}>
+            <Document
+              file={file}
+              onLoadSuccess={onDocumentLoadSuccess}
+              className="flex justify-center"
+            >
+              <Page pageNumber={pageNumber} scale={scale} />
+            </Document>
+          </section>
         </section>
         <section className="basis-2/5">
           <PDFSideBar onFileChange={onFileChange} />
@@ -222,3 +283,7 @@ const PDFReader = () => {
 };
 
 export default PDFReader;
+
+// export default dynamic(() => Promise.resolve(PDFReader), {
+//   ssr: false,
+// });
