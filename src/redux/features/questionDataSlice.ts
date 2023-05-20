ea40ed73a039ffc41ddddba64815510a06edcf56
Priyanshu-AI <priyanshu0627@gmail.com>
2023-05-20 21:19:46 +0530

    update add new question reducer redux

---
+++ src/redux/features/questionDataSlice.ts
@@ -0,0 +1,41 @@
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+type QuestionDataState = {
+  id: number;
+  upVotes: number;
+  downVotes: number;
+  liked: number;
+  accepted: boolean;
+  flag: number;
+  views: number;
+  question: string;
+  askedBy: string;
+  lastActivityTime: string;
+  lastActivityPerson: string;
+  tags: string[];
+};
+
+const initialState = [] as QuestionDataState[];
+
+export const questionData = createSlice({
+  name: 'questionData',
+  initialState,
+  reducers: {
+    reset: () => initialState,
+    addNewQuestion: (state, action) => {
+      // eslint-disable-next-line no-param-reassign
+      return [...state, action.payload.newQuestionData];
+    },
+    deleteQuestion: (state, action) => {
+      // eslint-disable-next-line no-param-reassign
+      console.log(state, action);
+    },
+    test: () => {},
+  },
+});
+
+export const { addNewQuestion, reset, deleteQuestion, test } =
+  questionData.actions;
+
+export default questionData.reducer;
